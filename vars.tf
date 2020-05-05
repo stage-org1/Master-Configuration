0fee04a75588b1bb1493e3e1ac03ae7eca6e27f6
beppe <beppe.vanrolleghem@gmail.com>
2020-05-05 15:41:04 +0200

    terraform files

---
+++ vars.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "europe-west1"
+}
+
+variable "zone" {
+  default = "europe-west1-b"
+}
+
+variable "prefix" {
+  default = "ff-stage-euw1-"
+}
+
+variable "creator" {
+  default = "beppe_vanrolleghem"
+}
+
+variable "machine_type" {
+  default = "n1-standard-1"
+}
